aa7da035dcadaa6f121e330a3ae9237d8d4c3fe4
naumtinga <naumtinga@gmail.com>
2024-06-27 08:50:18 +0200

    Cors added

--- projectDoacoes/settings.py
+++ projectDoacoes/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     "api",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -70,6 +72,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'projectDoacoes.wsgi.application'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',  # Replace with your frontend URL
+]
+
+# Optional: Allow all headers and methods
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 
 # Database
